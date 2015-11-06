f80f44e2c9135183a52755d07af029b0042ac80b
Antonia Cronin <attycool@hotmail.com>
2015-11-06 18:30:51 +0000

    minor changes

--- js/monsterBorn.js
+++ js/monsterBorn.js
@@ -4,38 +4,53 @@ $(document).ready(function() {
 
    // Array of images and text to show for story options
   var monsterStory = [ 
+  //0
       { story1: "it's a FARAWAY-IN-THE-FORESTS monster, or ... ", 
         story2: "it's an UNDER-YOUR-BED monster", 
-        //story3: "If it's a FARAWAY-IN-THE-FORESTS monster",
         b0: "If it's a FARAWAY-IN-THE-FORESTS monster ...",
         b1: "If it's an UNDER-YOUR-BED monster ...",
         bg1: "rgb(226,26,37)"},
-        
+//1        
       { story1: "it EATS YOU, or ...", 
         story2: "you make friends and TAKE IT TO SCHOOL.", 
-        //story3: "If it's a FARAWAY-IN-THE-FORESTS monster",
         b0: "If it EATS YOU ...",
         b1: "But if you TAKE IT TO SCHOOL ...",
         bg1: "#197278"},
-
+//2
       { story1: "it SITS QUIETLY, does its HOMEWORK and becomes the first monster to play for the SCHOOL BASKETBALL TEAM, or ...", 
         story2: "it eats the HEAD TEACHER.", 
-        //story3: "If it's a FARAWAY-IN-THE-FORESTS monster",
         b0: "If it SITS QUIETLY ...",
         b1: "But if it eats the HEAD TEACHER ... ",
-        bg1: "#31572C"}
-     
+        bg1: "#31572C"},
+//3
+      { story1: "it SITS QUIETLY, does its HOMEWORK and becomes the first monster to play for the SCHOOL BASKETBALL TEAM, or ...", 
+        story2: "it eats the HEAD TEACHER.", 
+        b0: "If it SITS QUIETLY ...",
+        b1: "But if it eats the HEAD TEACHER ... ",
+        bg1: "#rgb(226,26,37)"},
+//4
+      { story1: "it growls \"YUMMY!\" and DANCES BOOGIE-WOOGIE, or ...", 
+        story2: "it growls \"SORRY!\" and WALKS OFF through the wall.", 
+        b0: "If it DANCES BOOGIE-WOOGIE ...",
+        b1: "But if it WALKS OFF ... ",
+        bg1: "#197278"},
+//5
+      { story1: "it SITS IN THE PARK and scratches its head, or ...", 
+        story2: "it takes a deep breath and sets off for the FARAWAY-FORESTS.", 
+        b0: "If it SITS IN THE PARK ...",
+        b1: "But if it sets off for the FARAWAY-FORESTS ... ",
+        bg1: "#31572C"}     
     ];
   //variables for story
   var poss = "<h5>...there are two possibilities - </h5>";
   var either = " either ";
   var choice = "Make your choice!";
-  var intro ="<p>I was inspired to write a program while reading this wonderful book to my children, so I could practice my code and make them a game, at the same time. The story is about what happens when a monster is born and presents a series of boolean selections (there are two possibilities...). I would read the book and plan the code in my head while they fell asleep!</p>"
-  var github_link = "<a href='https://github.com/AttyC/Portfolio/blob/master/js/monsterBorn.js' target='_blank'>jQuery code on Github</a>";
-  
+  var intro ="<div class='start'><p>I was inspired to write a program while reading this wonderful book to my children, so I could practice my code and make them a game, at the same time. The story is about what happens when a monster is born and presents a series of boolean selections (there are two possibilities...). I would read the book and plan the code in my head while they fell asleep!</p></div>"
+  //var github_link = "<a href='https://github.com/AttyC/Portfolio/blob/master/js/monsterBorn.js' target='_blank'>jQuery code on Github</a><br/>";
+  //var amazon_link = "<a href='http://www.amazon.co.uk/When-A-Monster-Is-Born/dp/1846165113' target='_blank'>View Book on Amazon</a>";
   var i = 0; //set first screen
-  $("#monsterPic").html(github_link + "<a href='http://www.amazon.co.uk/When-A-Monster-Is-Born/dp/1846165113' target='_blank'><img src='img/monster.jpg' alt='When a monster is born'></a>" + intro + "<h3>When a Monster is Born...</h3>"  + poss + "<p>" + either + monsterStory[i].story1 + "<br/>" + monsterStory[i].story2 + "</p>" + choice);
-  $("#monsterPic").css("background", monsterStory[i].bg1);
+  $("#monsterPic").html(intro + "<h2>When a <span class='monster'>Monster</span> is Born...</h2>"  + poss + "<p>" + either + monsterStory[i].story1 + "<br/>" + monsterStory[i].story2 + "</p>" + choice);
+  //$("#monsterPic").css("background", monsterStory[i].bg1);
 
   //buttons for story choices - 0 or 1
   $('#button0').html("<span>" + monsterStory[i].b0 + "</span>");
